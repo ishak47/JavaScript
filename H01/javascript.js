da4c258471a2426d615f1912e37e322fc8f62c57
ishak <ijmilou@roc-dev.com>
2020-11-23 11:23:57 +0100

    opdracht 1.1 en 1.2 af

---
+++ H01/javascript.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+
+for (var i=0; i<10; i++){
+    console.log("de waarde van i is " +i)
+}
+
+var mijnauto ={
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
