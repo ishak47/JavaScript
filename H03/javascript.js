8716976a2ccc9c08ac623bf5c26832bbe0a64d58
ishak <ijmilou@roc-dev.com>
2020-11-24 13:34:18 +0100

    klaar met hoofdstuk 3

--- H03/javascript.js
+++ H03/javascript.js
@@ -1,2 +1,16 @@
-var plaatjes= document.getElementsByTagName("img")
-console.log ((Math.floor(Math.random()*9)+1));
+var plaatjes= document.getElementsByTagName("img");
+var random = 0;
+var randomNumber = [];
+
+while (randomNumber.length < 9){
+    random =Math.floor(Math.random() * 9) + 1;
+    if (randomNumber.lastIndexOf(random)== -1){
+        randomNumber.push(random);
+    }
+}
+
+random = 0;
+for (var plaatje in plaatjes){
+plaatjes[plaatje].src="img/logo" + randomNumber[random] + ".jpg";
+random++;
+}
