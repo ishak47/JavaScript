dab919c7648f40cc292a69293eacabba0667a59c
ishak <ijmilou@roc-dev.com>
2020-11-24 09:07:54 +0100

    opdracht 1 klaar

---
+++ H02/javascript.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText= "dit is de paginatitel";
+document.getElementById("subkop").innerText= "dit is de subkop";
+document.getElementById("premier").src="img/premier-league-logo-vector-380x380-1.jpg";
+document.getElementById("eredivisie").src="img/22922115912-thumb1.jpg";
