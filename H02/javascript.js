8b7de301fa06c5cd641d584a4cbd8d7eee9a49af
ishak <ijmilou@roc-dev.com>
2020-11-24 09:53:44 +0100

    h02 klaar

--- H02/javascript.js
+++ H02/javascript.js
@@ -2,3 +2,18 @@ document.getElementById("paginatitel").innerText= "dit is de paginatitel";
 document.getElementById("subkop").innerText= "dit is de subkop";
 document.getElementById("premier").src="img/premier-league-logo-vector-380x380-1.jpg";
 document.getElementById("eredivisie").src="img/22922115912-thumb1.jpg";
+
+
+//opdracht2
+document.getElementById("blok1").style.backgroundColor = "cyan";
+document.getElementById("blok1").style.textAlign="center";
+
+document.getElementById("blok2").style.backgroundColor = "green";
+document.getElementById("blok2").style.marginTop="25px";
+document.getElementById("blok2").style.textAlign="center";
+
+document.getElementById("blok3").style.backgroundColor = "yellow";
+document.getElementById("blok3").style.marginTop="50px"
+document.getElementById("blok3").style.textAlign="right";
+
+document.getElementById("blok4").style.backgroundColor = "red";
