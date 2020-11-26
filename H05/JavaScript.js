67d6ae366226fe5ef991d7e018d4e8d6c67a09b8
ishak <ijmilou@roc-dev.com>
2020-11-26 10:17:04 +0100

    hartjes willen niet in beeld komen

---
+++ H05/JavaScript.js
@@ -0,0 +1,35 @@
+pics = document.getElementById("pics");
+createPicsHolder();
+createClubImages();
+
+function createPicsHolder(){
+    for (var i =0; i<9; i++){
+    pictureHolder= document.createElement("div");
+    pictureHolder.className = "picture-holder";
+    pictureHolder.id - "picture-holder-" + i;
+    pics.appendChild(pictureHolder);
+    }
+}
+
+function createClubImages(){
+    pictureHolder = document.getElementsByClassName("picture-holder");
+    for (var i = 0; i < pictureHolder.length; i++){
+        favoriet = document.createElement("div");
+        favoriet.className = "favoriet";
+        favoriet.id = "favoriet" + (i+1);
+        clubPlaatje = document.createElement("img");
+        clubPlaatje.src = "img/logo" + (i+1) + ".jpg";
+        clubPlaatje.id = (i+1);
+        clubPlaatje.addEventListener("click", function (){
+            maakFavoriet(this.id);
+        });
+        pictureHolder[i].appendChild(favoriet);
+        pictureHolder[i].appendChild(clubPlaatje);
+    }
+}
+
+function maakFavoriet(id){
+    console.log("maak mij favo het gaat om logo " + id );
+    favoriet = document.getElementById("favoriet" + id);
+    favoriet.style.backgroundImage = "url ('img/heart.png')";
+}
